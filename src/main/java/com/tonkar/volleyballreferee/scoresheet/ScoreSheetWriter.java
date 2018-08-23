6c55e2a08e18c5bffe56f87bf69e01fcfbabe03e
GuillaumeVT <guillaume.von.tokarski@gmail.com>
2018-08-23 13:20:49 +0100

    Links in score sheet

--- src/main/java/com/tonkar/volleyballreferee/scoresheet/ScoreSheetWriter.java
+++ src/main/java/com/tonkar/volleyballreferee/scoresheet/ScoreSheetWriter.java
@@ -854,7 +854,7 @@ public class ScoreSheetWriter {
                 "      background-color: #f3f3f3;\n" +
                 "      padding: 8px;\n" +
                 "      margin: 8px;\n" +
-                "      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);\n" +
+                "      box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);\n" +
                 "    }\n" +
                 "    .div-title {\n" +
                 "      font-weight: 700;\n" +
