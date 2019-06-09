a1a10a3c7160715eca0aa3149b4841ca43b4b807
GuillaumeVT <guillaume.von.tokarski@gmail.com>
2019-06-09 13:58:20 +0100

    JDK 12 migration

--- run_api.sh
+++ run_api.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Dspring.profiles.active=production /opt/volleyball-referee-api-3.0.jar
+java -jar -Dspring.profiles.active=production --enable-preview /opt/volleyball-referee-api-3.0.jar
