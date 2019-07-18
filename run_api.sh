3bdeae06fc0433218134447537738cebc23e70de
GuillaumeVT <guillaume.von.tokarski@gmail.com>
2019-07-18 08:17:37 +0100

    Updated dependencies

--- run_api.sh
+++ run_api.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Djasypt.encryptor.password=? -Dspring.profiles.active=production --enable-preview /opt/volleyball-referee-api-3.1.jar
+java -jar -Djasypt.encryptor.password=${JASYPT_KEY} -Dspring.profiles.active=production --enable-preview /opt/volleyball-referee-api-3.1.jar
